3c732b10cfde045eeffd89dbb572d06136c5758c
Giovanni <barbierig25@yahoo.com.br>
2025-05-14 23:18:20 -0300

    ✨ feat: Enhance SaveButton component with customizable text props - Added optional text and pendingText props to SaveButton - Improved button rendering based on pending state

--- frontend/src/app/components/forms/SaveButton.tsx
+++ frontend/src/app/components/forms/SaveButton.tsx
@@ -1,14 +1,19 @@
 "use client";
 import { useFormStatus } from "react-dom";
 
-export default function SaveButton() {
+interface SaveButtonProps {
+    text?: string;
+    pendingText?: string;
+}
+
+export default function SaveButton({ text = "Salvar", pendingText = "Salvando..." }: SaveButtonProps) {
     const { pending } = useFormStatus();
     return (
         <button
             type="submit"
             disabled={pending}
-            className="bg-primary-400 w-28 px-6 py-1.5 text-white text-base font-medium shadow-md rounded-md not-disabled:hover:bg-primary-500 cursor-pointer disabled:cursor-not-allowed disabled:bg-primary-800 disabled:animate-pulse">
-            {pending ? "Salvando..." : "Salvar"}
+            className="bg-primary-400 w-28 px-6 py-1.5 text-white text-base text-center font-medium shadow-md rounded-md not-disabled:hover:bg-primary-500 cursor-pointer disabled:cursor-not-allowed disabled:bg-primary-800 disabled:animate-pulse">
+            {pending ? pendingText : text}
         </button>
     );
 }
